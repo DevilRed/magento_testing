19f2bfb6230d8ca18da4eaa0e7b4eee700d77b2b
chris <chrisdaq@gmail.com>
2015-10-21 16:37:30 -0400

    floorplans products collection showed by position in frontend grid tab, all in admin backend

--- app/code/local/Validoc/Floorplan/Block/Adminhtml/Floorplan/Edit/Tab/Customization.php
+++ app/code/local/Validoc/Floorplan/Block/Adminhtml/Floorplan/Edit/Tab/Customization.php
@@ -10,25 +10,42 @@
 			$productsData = $floorplan->getSelectedProducts();
 			$floorplanProducts = array();
 			foreach ($productsData as $product) {
-				$floorplanProducts[$product->_data["entity_id"]] = $product->_data["quantity"];
+				$floorplanProducts[$product->_data["entity_id"]] = array('quantity' => $product->_data["quantity"], 'position' => $product->_data['position']);
 				//var_dump($product);
 			}
+			//reorder array based on position
+			$floorplanProducts = $this->orderBy($floorplanProducts, 'position');
+
 			$productsId = array();
 			foreach ($floorplanProducts as $id => $qty) {
 				array_push($productsId, $id);
 			}
+			$orderString = array('CASE e.entity_id');
+			foreach($productsId as $i => $productId){
+				$orderString[] = 'WHEN '.$productId.' THEN '.$i;
+			}
+			$orderString[] = ' END';
+			$orderString = implode(' ', $orderString);
 			$attributes = Mage::getSingleton('catalog/config')->getProductAttributes();
 			$_productCollection = Mage::getModel('catalog/product')
 										->getCollection()
-										->addAttributeToSort('created_at', 'DESC')
+										/*->addAttributeToSort('created_at', 'DESC')*/
 				                        ->addAttributeToFilter('entity_id',  array('in' => $productsId))
-				                        ->addAttributeToSelect($attributes)
-				                        ->load();
+				                        ->addAttributeToSelect($attributes);
+				                        //->load();
+			$_productCollection->getSelect()->order(new Zend_Db_Expr($orderString));
+			$_productCollection->load();
 			return $_productCollection;
 		}
 		public function getGrid($floorplanId){
 			$model = Mage::getModel('validoc_floorplan/floorplan');
 			$floorplan = $model->load($floorplanId);
 			return $floorplan->getSerializedGrid();
+		}
+		private function orderBy($data, $field){
+			$code = "return strnatcmp(\$a['$field'], \$b['$field']);";
+			uasort($data, create_function('$a,$b', $code));
+
+			return $data;
 		}
 	}
