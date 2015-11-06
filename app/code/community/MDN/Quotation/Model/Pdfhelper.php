009626ccf175b87d85fde677e7e25695f19834a0
chris <chrisdaq@gmail.com>
2015-11-06 12:22:43 -0400

    settings for pdf

--- app/code/community/MDN/Quotation/Model/Pdfhelper.php
+++ app/code/community/MDN/Quotation/Model/Pdfhelper.php
@@ -129,7 +129,7 @@ abstract class MDN_Quotation_Model_Pdfhelper extends Mage_Sales_Model_Order_Pdf_
         $page->drawRectangle(10, $this->_BLOC_FOOTER_HAUTEUR + 15, $this->_BLOC_FOOTER_LARGEUR, 15, Zend_Pdf_Page::SHAPE_DRAW_FILL);
 
         //text
-        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0.2));
         $this->DrawFooterMultilineText($page, Mage::getStoreConfig('quotation/pdf/pdf_footer', $StoreId), 20, $this->_BLOC_FOOTER_HAUTEUR, 10, 0, 15);
     }
 
